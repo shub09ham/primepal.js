54a2d9cf2e769c9163de6f95f94d184072c4e9aa
shub09ham <110715617+shub09ham@users.noreply.github.com>
2022-08-24 18:41:21 +0530

    Add files via upload

---
+++ primepal.js
@@ -0,0 +1,31 @@
+
+// A function to check if n is palindrome
+function isPalindrome(n)
+{
+	// Find reverse of n
+	var rev = 0;
+	for (var i = n; Math.trunc(i) > 0; i /= 10)
+	{
+		rev = ((rev*10) + (Math.trunc(i)%10));
+		
+		}
+	
+	// If n and rev are same, then n is palindrome
+	return (n==rev);
+}
+	
+
+// prints palindrome between min and max
+function countPal(min, max)
+{
+	for (var i = min; i <=max; i++)
+	{
+		if(isPalindrome(i))
+		document.write(i+" " );
+	}
+}
+
+// Driver program to test above function
+
+	countPal(100, 2000);
+
